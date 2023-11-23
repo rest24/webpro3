b9aefc664b5809c75e8c5a93033a0cba4775667f
rest24 <restu.andriyana.suhendar@gmail.com>
2023-11-23 19:41:35 +0700

    Nambah css

--- application/views/addanggota.php
+++ application/views/addanggota.php
@@ -4,6 +4,48 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Tambah Anggota</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+			background-color: #f4f4f4;
+		}
+		form {
+			background-color: #fff;
+			padding: 20px;
+			border-radius: 8px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+		table {
+			width: 100%;
+		}
+		td {
+			padding: 8px;
+		}
+		input[type="text"],
+		input[type="password"],
+		input[type="submit"] {
+			width: 100%;
+			padding: 8px;
+			margin-bottom: 10px;
+			border-radius: 4px;
+			border: 1px solid #ccc;
+			box-sizing: border-box;
+		}
+		input[type="submit"] {
+			background-color: #007bff;
+			color: #fff;
+			cursor: pointer;
+		}
+		input[type="submit"]:hover {
+			background-color: #0056b3;
+		}
+	</style>
 </head>
 <body>
 <form action="<?php echo base_url().'anggota/simpan';?>" method="POST">
--- application/views/viewanggota.php
+++ application/views/viewanggota.php
@@ -4,6 +4,36 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Anggota</title>
+	<style>
+		/* Gaya untuk tabel */
+		table {
+			width: 100%;
+			border-collapse: collapse;
+		}
+		table, th, td {
+			border: 1px solid black;
+		}
+		th, td {
+			padding: 8px;
+			text-align: left;
+		}
+		th {
+			background-color: #f2f2f2;
+		}
+		/* Gaya untuk tautan tambah data */
+		.add-link {
+			display: block;
+			margin-bottom: 15px;
+			text-decoration: none;
+			padding: 8px 16px;
+			background-color: #007bff;
+			color: #fff;
+			border-radius: 4px;
+		}
+		.add-link:hover {
+			background-color: #0056b3;
+		}
+	</style>
 </head>
 <body>
 <a href="<?php echo base_url().'anggota/tambah';?>">Tambah Data</a>
